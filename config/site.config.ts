ec5c12de14366bcb5aae371b1111f3809f7969b8
seghirabdo5-jpg <seghirabdo5@gmail.com>
2026-08-23 15:23:56 +0100

    Update site.config.ts

--- config/site.config.ts
+++ config/site.config.ts
@@ -19,7 +19,13 @@ export const goldPriceConfig = {
 };
 
 export const affiliateLinks: Record<string, string> = {
-  zomorod: "",
+  zomorod: "export const affiliateLinks: Record<string, string> = {
+  zomorod: "https://zomorodjew.com/?a_aid=0xl4b7gu1x2zl&a_bid=6cbb7f02",
+  alrabsh: "https://alarbashjew.com/?utm_source=linkaraby&utm_medium=referral&a_aid=0xl4b7gu1x2zl&a_bid=2e2f8972",
+  "colin-cleef": "",
+  alghunaim: "https://alghunaim-jewelry.com/category/BqDwYD?a_aid=0xl4b7gu1x2zl&a_bid=53c943a9",
+  "onyx-rose": "https://onexrose.com/?a_aid=0xl4b7gu1x2zl&a_bid=47d0ce85",
+};",
   alrabsh: "export const affiliateLinks: Record<string, string> = {
   zomorod: "",
   alrabsh: "https://alarbashjew.com/?utm_source=linkaraby&utm_medium=referral&a_aid=0xl4b7gu1x2zl&a_bid=2e2f8972",
